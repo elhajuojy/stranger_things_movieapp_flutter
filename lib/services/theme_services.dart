1648d21cc12c27d696d6dba469282bda4d103b7e
elhajuojy <souirimehdi311@gmail.com>
2022-08-26 15:45:59 +0100

    theme service

---
+++ lib/services/theme_services.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ThemeService {
+  final _box = GetStorage();
+  final _key = "isDarkMode";
+  //if there a value return that value as true is there nothing return false
+  bool _loadThemeFromBox() => _box.read(_key) ?? false;
+
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+
+  void switchTheme() {
+    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
+  }
+}
