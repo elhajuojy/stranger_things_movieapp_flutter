d807739e9a80cae443fbbfce60174a68d0d6de67
elhajuojy <souirimehdi311@gmail.com>
2022-08-26 15:45:45 +0100

    color

---
+++ lib/core/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static Color primaryBlueColor = const Color(0xFF0068ff);
+  static Color backgroundDarkColor = const Color(0xFF1b1c21);
+  static Color backgroundlightColor = const Color(0xffffffff);
+  static Color navgationBackgroundColor = const Color(0xFF28292e);
+}
